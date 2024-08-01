2476c644d5f43103b43f2e80cc0be5ffa0bcf4b7
Anton <ant1kow22@gmail.com>
2024-08-01 15:01:46 +0300

    add ownerId field

--- SharingMessages/ThingModel.cs
+++ SharingMessages/ThingModel.cs
@@ -2,7 +2,9 @@
 
 public class ThingModel
 {
+    public Guid ThingId { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
     public required double Price { get; set; }
+    public Guid OwnerId { get; set; }
 }
